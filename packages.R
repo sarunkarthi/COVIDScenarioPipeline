c46641e793211b231af13e6768f213b5803b5aca
Josh Wills <jwills@apache.org>
2020-03-17 08:59:22 -0700

    fix this so it doesn't re-install stuff that is already installed

--- packages.R
+++ packages.R
@@ -1,1 +1,3 @@
-install.packages(c("Rcpp", "tidyr", "dplyr", "readr", "ggplot2", "gridExtra", "sf", "viridis", "ggfortify", "flextable", "cowplot"))
+list.of.packages <- c("Rcpp", "tidyr", "dplyr", "readr", "ggplot2", "gridExtra", "sf", "viridis", "ggfortify", "flextable", "cowplot")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
